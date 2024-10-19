46011c3f28239de57d8a02d051dcfe0557a2c34c
lui5gl <lui5gl@outlook.com>
2024-10-19 16:57:21 -0600

    feat: add button drop shadow to tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,7 @@ const config: Config = {
     extend: {
       boxShadow: {
         box: "10px 10px 0 rgb(255 255 255 / 0.1)",
+        button: "5px 5px 0 rgb(255 255 255 / 0.1)",
       },
       dropShadow: {
         font: "5px 5px 0 rgb(255 255 255 / 0.25)",
