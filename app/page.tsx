97911ef48d8efd83c8188bb4862e2c58aae43ffc
lui5gl <lui5gl@outlook.com>
2024-11-18 14:27:11 -0600

    Add notification button to enable sound alerts for breaks

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Image from "next/image";
 import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
@@ -15,6 +16,8 @@ export default function Home() {
         : 60 - current_minute
   );
   const notification_sound = useRef<HTMLAudioElement>(null);
+  const notification_button = useRef<HTMLButtonElement>(null);
+  const [notificationsEnabled, setNotificationsEnabled] = useState(false);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -31,7 +34,8 @@ export default function Home() {
             : 60 - current_minute
       );
 
-      if (next_break.toString() === "20") notification_sound.current?.play();
+      if (notificationsEnabled && next_break.toString() === "0")
+        notification_sound.current?.play();
     }, 1000);
 
     return () => clearInterval(interval);
@@ -39,6 +43,21 @@ export default function Home() {
 
   return (
     <main className="flex flex-col items-center justify-center min-h-screen gap-5 bg-gradient-to-br from-neutral-600 to-neutral-800">
+      <button
+        ref={notification_button}
+        onClick={() => {
+          notification_button.current?.classList.add("hidden");
+          setNotificationsEnabled(true);
+        }}
+        className="bg-neutral-100 p-2 rounded-sm fixed top-5 right-5 hover:drop-shadow-font transition-all duration-150"
+      >
+        <Image
+          src="/images/bell_icon.png"
+          alt="bell icon"
+          width={24}
+          height={24}
+        />
+      </button>
       <h1 className="px-8 py-4 text-5xl font-bold rounded-sm text-neutral-800 shadow-box bg-neutral-100">
         Care for your eyes
       </h1>
