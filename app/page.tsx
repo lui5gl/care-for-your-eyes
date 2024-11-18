e7d846b9d5af7a00766a049051b26157d9e796a8
lui5gl <lui5gl@outlook.com>
2024-11-17 20:15:52 -0600

    Refactored code for simplification and updated design to a simpler, larger layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,79 +1,53 @@
 "use client";
-import Image from "next/image";
 import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
-  let hour_now = new Date().getHours();
-  let minute_now = new Date().getMinutes();
+  let current_hour = new Date().getHours();
+  let current_minute = new Date().getMinutes();
 
-  let remaining_time =
-    minute_now <= 20
-      ? 20 - minute_now
-      : minute_now <= 40
-        ? 40 - minute_now
-        : 60 - minute_now;
-
-  const [hour, setHour] = useState(hour_now);
-  const [minute, setMinute] = useState(minute_now);
-  const [next_break, setNextBreak] = useState(remaining_time);
-  const [notificationEnabled, setNotificationEnabled] = useState(false);
-
+  const [hour, SetHour] = useState(current_hour);
+  const [minute, SetMinute] = useState(current_minute);
+  const [next_break, SetNextBreak] = useState(
+    current_minute <= 20
+      ? 20 - current_minute
+      : current_minute <= 40
+        ? 40 - current_minute
+        : 60 - current_minute
+  );
   const notification_sound = useRef<HTMLAudioElement>(null);
-  const notificationButtonRef = useRef<HTMLButtonElement>(null);
 
   useEffect(() => {
-    const intervalId = setInterval(() => {
-      let hour_now = new Date().getHours();
-      let minute_now = new Date().getMinutes();
+    const interval = setInterval(() => {
+      let current_hour = new Date().getHours();
+      let current_minute = new Date().getMinutes();
 
-      let remaining_time =
-        minute_now <= 20
-          ? 20 - minute_now
-          : minute_now <= 40
-            ? 40 - minute_now
-            : 60 - minute_now;
+      SetHour(current_hour);
+      SetMinute(current_minute);
+      SetNextBreak(
+        current_minute <= 20
+          ? 20 - current_minute
+          : current_minute <= 40
+            ? 40 - current_minute
+            : 60 - current_minute
+      );
 
-      setHour(hour_now);
-      setMinute(minute_now);
-      setNextBreak(remaining_time);
-
-      if (remaining_time === 20 && notificationEnabled === true) {
-        notification_sound.current?.play();
-      }
+      if (next_break.toString() === "20") notification_sound.current?.play();
     }, 1000);
 
-    return () => clearInterval(intervalId);
-  }, [notificationEnabled]);
-
-  const handleNotificationEnable = () => {
-    setNotificationEnabled(true);
-    notificationButtonRef.current?.classList.add("hidden");
-  };
+    return () => clearInterval(interval);
+  }, []);
 
   return (
-    <main className="grid min-h-screen gap-2 text-white select-none bg-gradient-to-t to-stone-500 from-stone-700 place-content-center">
-      <h1 className="text-4xl font-bold text-center">Care for your 👀</h1>
-      <section className="grid p-5 rounded-md bg-white/25 shadow-box">
-        <p className="font-bold text-center text-9xl drop-shadow-font">
-          {hour < 10 ? "0" + hour : hour}:{minute < 10 ? "0" + minute : minute}
-        </p>
-        <span className="text-lg font-semibold text-center animate-pulse">
-          Next break in {next_break} minutes 🙀
-        </span>
-        <button
-          ref={notificationButtonRef}
-          className="flex items-center justify-center gap-2 py-1 mt-2 transition-all duration-200 rounded-md bg-white/25 hover:shadow-button active:translate-x-1 active:translate-y-1 active:shadow-none"
-          onClick={handleNotificationEnable}
-        >
-          Enable notifications
-          <Image
-            src="/images/bell_icon.png"
-            alt="bell icon"
-            height={12}
-            width={12}
-          />
-        </button>
-      </section>
+    <main className="flex flex-col items-center justify-center min-h-screen gap-5 bg-gradient-to-br from-neutral-600 to-neutral-800">
+      <h1 className="px-8 py-4 text-5xl font-bold rounded-sm text-neutral-800 shadow-box bg-neutral-100">
+        Care for your eyes
+      </h1>
+      <span className="font-bold text-neutral-100 text-9xl drop-shadow-font">
+        {hour < 10 ? "0" + hour : hour}:{minute < 10 ? "0" + minute : minute}
+      </span>
+      <span className="text-xl font-semibold text-neutral-100 animate-pulse">
+        Next break in {next_break} minutes
+      </span>
       <audio src="/sounds/bell_sound.mp3" ref={notification_sound} />
     </main>
   );
