1975bdcdb34eb64cd70f9968f15a272980364615
lui5gl <lui5gl@outlook.com>
2024-12-05 14:40:02 -0600

    Fix formatting and minor styling adjustments in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   const [hour, SetHour] = useState(new Date().getHours());
   const [minute, SetMinute] = useState(new Date().getMinutes());
   const [next_break, SetNextBreak] = useState(
-    minute <= 20 ? 20 - minute : minute <= 40 ? 40 - minute : 60 - minute
+    minute <= 20 ? 20 - minute : minute <= 40 ? 40 - minute : 60 - minute,
   );
 
   const [isSoundAllowed, setIsSoundAllowed] = useState(false);
@@ -21,7 +21,7 @@ export default function Home() {
       SetHour(new Date().getHours());
       SetMinute(new Date().getMinutes());
       SetNextBreak(
-        minute <= 20 ? 20 - minute : minute <= 40 ? 40 - minute : 60 - minute
+        minute <= 20 ? 20 - minute : minute <= 40 ? 40 - minute : 60 - minute,
       );
 
       if (next_break === 0) notification_sound.current?.play();
@@ -31,19 +31,19 @@ export default function Home() {
   }, []);
 
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen gap-5 bg-gradient-to-br from-neutral-600 to-neutral-800">
-      <h1 className="px-8 py-4 text-5xl font-bold rounded-sm text-neutral-800 shadow-box bg-neutral-100">
+    <main className="flex min-h-screen flex-col items-center justify-center gap-5 bg-gradient-to-br from-neutral-600 to-neutral-800">
+      <h1 className="rounded-sm bg-neutral-100 px-8 py-4 text-5xl font-bold text-neutral-800 shadow-box">
         Care for your eyes
       </h1>
-      <span className="font-bold text-neutral-100 text-9xl drop-shadow-font">
+      <span className="drop-shadow-font text-9xl font-bold text-neutral-100">
         {hour < 10 ? "0" + hour : hour}:{minute < 10 ? "0" + minute : minute}
       </span>
-      <span className="text-xl font-semibold text-neutral-100 animate-pulse">
+      <span className="animate-pulse text-xl font-semibold text-neutral-100">
         Next break in {next_break} minutes
       </span>
       {!isSoundAllowed && (
         <button
-          className="bg-neutral-100 p-1 rounded-sm hover:shadow-button font-semibold text-neutral-800 px-4 py-2 flex gap-1 justify-center items-center"
+          className="flex items-center justify-center gap-1 rounded-sm bg-neutral-100 p-1 px-4 py-2 font-semibold text-neutral-800 hover:shadow-button"
           onClick={() => setIsSoundAllowed(true)}
         >
           <Image src="/icons/bell.png" alt="Bell" width={16} height={16} />
