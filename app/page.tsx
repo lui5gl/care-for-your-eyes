cf6ca6a3e4f808d332e73fc4a50722ed0281417b
lui5gl <lui5gl@outlook.com>
2024-12-05 14:01:17 -0600

    Refactor time calculation logic and remove unused notification button

--- app/page.tsx
+++ app/page.tsx
@@ -1,41 +1,25 @@
 "use client";
-import Image from "next/image";
+
 import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
-  let current_hour = new Date().getHours();
-  let current_minute = new Date().getMinutes();
-
-  const [hour, SetHour] = useState(current_hour);
-  const [minute, SetMinute] = useState(current_minute);
-  const [next_break, SetNextBreak] = useState(
-    current_minute <= 20
-      ? 20 - current_minute
-      : current_minute <= 40
-        ? 40 - current_minute
-        : 60 - current_minute
-  );
   const notification_sound = useRef<HTMLAudioElement>(null);
-  const notification_button = useRef<HTMLButtonElement>(null);
-  const [notificationsEnabled, setNotificationsEnabled] = useState(false);
+
+  const [hour, SetHour] = useState(new Date().getHours());
+  const [minute, SetMinute] = useState(new Date().getMinutes());
+  const [next_break, SetNextBreak] = useState(
+    minute <= 20 ? 20 - minute : minute <= 40 ? 40 - minute : 60 - minute
+  );
 
   useEffect(() => {
     const interval = setInterval(() => {
-      let current_hour = new Date().getHours();
-      let current_minute = new Date().getMinutes();
-
-      SetHour(current_hour);
-      SetMinute(current_minute);
+      SetHour(new Date().getHours());
+      SetMinute(new Date().getMinutes());
       SetNextBreak(
-        current_minute <= 20
-          ? 20 - current_minute
-          : current_minute <= 40
-            ? 40 - current_minute
-            : 60 - current_minute
+        minute <= 20 ? 20 - minute : minute <= 40 ? 40 - minute : 60 - minute
       );
 
-      if (notificationsEnabled && next_break.toString() === "0")
-        notification_sound.current?.play();
+      if (next_break === 0) notification_sound.current?.play();
     }, 1000);
 
     return () => clearInterval(interval);
@@ -43,21 +27,6 @@ export default function Home() {
 
   return (
     <main className="flex flex-col items-center justify-center min-h-screen gap-5 bg-gradient-to-br from-neutral-600 to-neutral-800">
-      <button
-        ref={notification_button}
-        onClick={() => {
-          notification_button.current?.classList.add("hidden");
-          setNotificationsEnabled(true);
-        }}
-        className="bg-neutral-100 p-2 rounded-sm fixed top-5 right-5 hover:drop-shadow-font transition-all duration-150"
-      >
-        <Image
-          src="/images/bell_icon.png"
-          alt="bell icon"
-          width={24}
-          height={24}
-        />
-      </button>
       <h1 className="px-8 py-4 text-5xl font-bold rounded-sm text-neutral-800 shadow-box bg-neutral-100">
         Care for your eyes
       </h1>
