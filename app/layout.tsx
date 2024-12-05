ecefca14df972dfd24a6103b030fd7a9cc19eb64
lui5gl <lui5gl@outlook.com>
2024-12-05 14:13:23 -0600

    Update app description to focus on core functionality and remove redundancy

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ const onest = Onest({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Care for your eyes",
   description:
-    "Care for Your Eyes is an app designed to promote eye care for individuals who spend extended periods in front of computer screens. The app's primary goal is to remind you to take regular breaks to prevent eye strain and maintain the health of your eyes. Following the 20-20-20 rule, the app will notify you every 20 minutes to focus your gaze on distant objects for 20 seconds. With an intuitive and customizable interface, Care for Your Eyes helps you develop healthy visual habits, reducing the risk of issues such as digital eye strain.",
+    "Care for Your Eyes is an app designed to promote eye care for individuals who spend extended periods in front of computer screens. The app's primary goal is to remind you to take regular breaks to prevent eye strain and maintain the health of your eyes. Following the 20-20-20 rule, the app will notify you every 20 minutes to focus your gaze on distant objects for 20 seconds.",
 };
 
 export default function RootLayout({
