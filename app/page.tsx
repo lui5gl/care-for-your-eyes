d1b45de5dcb077da8ff3651be9eb1ddc4c9043d1
lui5gl <lui5gl@outlook.com>
2024-12-25 16:04:27 -0600

    Refactor code, fix clock issue, add notification message

--- app/page.tsx
+++ app/page.tsx
@@ -4,53 +4,62 @@ import Image from "next/image";
 import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
-  const notification_sound = useRef<HTMLAudioElement>(null);
+  const now = new Date();
+  const [hour, setHour] = useState(now.getHours());
+  const [minute, setMinute] = useState(now.getMinutes());
 
-  const [hour, SetHour] = useState(new Date().getHours());
-  const [minute, SetMinute] = useState(new Date().getMinutes());
-  const [next_break, SetNextBreak] = useState(
+  const [next_break, setNextBreak] = useState(
     minute <= 20 ? 20 - minute : minute <= 40 ? 40 - minute : 60 - minute,
   );
 
   const [isSoundAllowed, setIsSoundAllowed] = useState(false);
+  const notification_sound_ref = useRef<HTMLAudioElement>(null);
 
   useEffect(() => {
-    if (!isSoundAllowed) return;
-
     const interval = setInterval(() => {
-      SetHour(new Date().getHours());
-      SetMinute(new Date().getMinutes());
-      SetNextBreak(
+      const now = new Date();
+
+      setHour(now.getHours());
+      setMinute(now.getMinutes());
+      setNextBreak(
         minute <= 20 ? 20 - minute : minute <= 40 ? 40 - minute : 60 - minute,
       );
 
-      if (next_break === 0) notification_sound.current?.play();
+      if (isSoundAllowed && next_break == 0)
+        notification_sound_ref.current?.play();
     }, 1000);
 
     return () => clearInterval(interval);
-  }, [isSoundAllowed, minute, next_break]);
+  }, []);
 
   return (
-    <main className="flex min-h-screen flex-col select-none items-center justify-center gap-5 bg-gradient-to-br from-neutral-600 to-neutral-800">
+    <main className="flex min-h-screen select-none flex-col items-center justify-center gap-3 bg-gradient-to-br from-neutral-600 to-neutral-800">
       <h1 className="rounded-sm bg-neutral-100 px-8 py-4 text-5xl font-bold text-neutral-800 shadow-box">
         Care for your eyes
       </h1>
       <span className="drop-shadow-font text-9xl font-bold text-neutral-100">
-        {hour < 10 ? "0" + hour : hour}:{minute < 10 ? "0" + minute : minute}
+        {hour.toString().padStart(2, "0")}:{minute.toString().padStart(2, "0")}
       </span>
       <span className="animate-pulse text-xl font-semibold text-neutral-100">
         Next break in {next_break} minutes
       </span>
+
       {!isSoundAllowed && (
-        <button
-          className="flex items-center justify-center gap-1 rounded-sm bg-neutral-100 p-1 px-4 py-2 font-semibold text-neutral-800 hover:shadow-button transition-all duration-150 active:translate-x-1 active:translate-y-1 active:shadow-none"
-          onClick={() => setIsSoundAllowed(true)}
-        >
-          <Image src="/icons/bell.svg" alt="Bell" width={16} height={16} />
-          Enable sound
-        </button>
+        <div className="group absolute left-5 top-5 flex items-center justify-center gap-5">
+          <button
+            className="flex items-center justify-center rounded-sm bg-neutral-50 p-2 transition-all duration-150 hover:shadow-button active:translate-x-1 active:translate-y-1 active:shadow-none"
+            onClick={() => setIsSoundAllowed(true)}
+          >
+            <Image src="/icons/bell.svg" alt="Bell" width={18} height={18} />
+          </button>
+          <div className="animate-pulse text-neutral-50/75 transition-all duration-150 group-hover:animate-none group-hover:opacity-0">
+            This site requires you to enable notifications every time the page
+            is reloaded.
+          </div>
+        </div>
       )}
-      <audio src="/sounds/bell_sound.mp3" ref={notification_sound} />
+
+      <audio src="/sounds/bell_sound.mp3" ref={notification_sound_ref} />
     </main>
   );
 }
