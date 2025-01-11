7def76b3a4c9953f6a816d2a72d44e83a82c45f1
lui5gl <lui5gl@outlook.com>
2025-01-11 11:25:30 -0600

    Refactor color scheme in Home component for improved accessibility and consistency

--- app/page.tsx
+++ app/page.tsx
@@ -39,28 +39,27 @@ export default function Home() {
   }, [isSoundAllowed, minute, nextBreak]);
 
   return (
-    <main className="flex min-h-screen select-none flex-col items-center justify-center gap-3 bg-gradient-to-br from-neutral-600 to-neutral-800">
-      <h1 className="rounded-sm bg-neutral-100 px-8 py-4 text-5xl font-bold text-neutral-800 shadow-box">
+    <main className="flex min-h-screen select-none flex-col items-center justify-center gap-3 bg-gradient-to-b from-zinc-600 to-zinc-800">
+      <h1 className="rounded-sm bg-zinc-100 px-8 py-4 text-5xl font-bold text-zinc-800 shadow-box">
         Care for your eyes
       </h1>
-      <span className="drop-shadow-font text-9xl font-bold text-neutral-100 drop-shadow-text">
+      <span className="drop-shadow-font text-9xl font-bold text-zinc-100 drop-shadow-text">
         {hour.toString().padStart(2, "0")}:{minute.toString().padStart(2, "0")}
       </span>
-      <span className="animate-pulse text-xl font-semibold text-neutral-100">
+      <span className="animate-pulse text-xl font-semibold text-zinc-100">
         Next break in {nextBreak} minutes
       </span>
 
       {!isSoundAllowed && (
-        <div className="group absolute left-5 top-5 flex items-center justify-center gap-5">
+        <div className="group absolute left-5 top-5 flex items-center justify-center gap-2">
           <button
-            className="flex items-center justify-center rounded-sm bg-neutral-50 p-2 transition-all duration-150 hover:shadow-button active:translate-x-1 active:translate-y-1 active:shadow-none"
+            className="flex items-center justify-center rounded-sm bg-zinc-100 p-2 transition-all duration-150 hover:shadow-button active:translate-x-1 active:translate-y-1 active:shadow-none"
             onClick={() => setIsSoundAllowed(true)}
           >
             <Image src="/icons/bell.svg" alt="Bell" width={18} height={18} />
           </button>
-          <div className="animate-pulse text-neutral-50/75 transition-all duration-150 group-hover:animate-none group-hover:opacity-0">
-            This site requires you to enable notifications every time the page
-            is reloaded.
+          <div className="animate-pulse text-zinc-50/75 transition-all duration-150 group-hover:animate-none group-hover:opacity-0">
+            This site needs notifications enabled.
           </div>
         </div>
       )}
