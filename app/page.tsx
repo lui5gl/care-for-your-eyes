e044eee1cde8b2a603bec4e496131ed59e6935f8
lui5gl <lui5gl@outlook.com>
2025-01-14 12:24:36 -0600

    Simplify title update logic in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -40,10 +40,7 @@ export default function Home() {
 
   useEffect(() => {
     let title = "Care for your eyes";
-
-    if (!isSoundAllowed) {
-      title = "🔇 " + title;
-    }
+    if (!isSoundAllowed) title = "🔇 " + title;
 
     document.title = title;
   }, [isSoundAllowed]);
